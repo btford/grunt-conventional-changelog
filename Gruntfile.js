f842aac20e46d79a9f31bb389f0df4bfcac0eb4b
Jonas Einhorn <jonas.wittlin@daseinhorn.ch>
2013-10-19 22:59:44 +0200

    feat(bitbucket): make bitbucket work
    Implemented bitbucket because not only github users should be able to use the commit and issue link feature.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,52 +1,52 @@
 'use strict';
-module.exports = function (grunt) {
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    jshint: {
-      options: {
-        jshintrc: '.jshintrc'
-      },
-      all: [
-        'Gruntfile.js',
-        'lib/*.js',
-        'tasks/*.js',
-        'test/**/*.js'
-      ]
-    },
-    release: {
-      options: {
-        commitMessage: 'v<%= version %>',
-        tagName: 'v<%= version %>'
-      }
-    },
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc'
+            },
+            all: [
+                'Gruntfile.js',
+                'lib/*.js',
+                'tasks/*.js',
+                'test/**/*.js'
+            ]
+        },
+        release: {
+            options: {
+                commitMessage: 'v<%= version %>',
+                tagName: 'v<%= version %>'
+            }
+        },
 
-    simplemocha: {
-      options: {
-        ui: 'bdd',
-        reporter: 'dot'
-      },
-      unit: {
-        src: [
-          'test/**/*.coffee'
-        ]
-      }
-    },
+        simplemocha: {
+            options: {
+                ui: 'bdd',
+                reporter: 'dot'
+            },
+            unit: {
+                src: [
+                    'test/**/*.coffee'
+                ]
+            }
+        },
 
-    watch: {
-      unittests: {
-        files: ['test/**/*.coffee', 'lib/**/*.js'],
-        tasks: ['test']
-      }
-    }
-  });
+        watch: {
+            unittests: {
+                files: ['test/**/*.coffee', 'lib/**/*.js'],
+                tasks: ['test']
+            }
+        }
+    });
 
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-release');
-  grunt.loadNpmTasks('grunt-simple-mocha');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-release');
+    grunt.loadNpmTasks('grunt-simple-mocha');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.loadTasks('tasks');
+    grunt.loadTasks('tasks');
 
-  grunt.registerTask('default', ['jshint', 'test']);
-  grunt.registerTask('test', ['simplemocha']);
+    grunt.registerTask('default', ['jshint', 'test']);
+    grunt.registerTask('test', ['simplemocha']);
 };
--- tasks/changelog.js
+++ tasks/changelog.js
@@ -3,72 +3,97 @@ var changelog = require('../lib/changelog');
 
 
 // TODO: clean this up and write tests for it
-var figureOutGithubRepo = function(githubRepo, pkg) {
-
-  //If github repo isn't given, try to read it from the package file
-  if (!githubRepo && pkg) {
-    if (pkg.repository) {
-      githubRepo = pkg.repository.url;
-    } else if (pkg.homepage) {
-      //If it's a github page, but not a *.github.(io|com) page
-      if (pkg.homepage.indexOf('github.com') > -1 &&
-          !pkg.homepage.match(/\.github\.(com|io)/)) {
-        githubRepo = pkg.homepage;
-      }
+var figureOutRepo = function(githubRepo, bitbucketRepo, pkg) {
+    //Check if the user gave either a github or a bitbucket url
+    var type = 0,
+        repoUrl = "";
+    if (githubRepo) {
+        console.log("1");
+        repoUrl = githubRepo;
+        bitbucketRepo = "";
+    } else if (bitbucketRepo) {
+        type = 2;
+        repoUrl = bitbucketRepo;
+        githubRepo = "";
     }
-  }
 
-  //User could set option eg 'github: "btford/grunt-conventional-changelog'
-  if (githubRepo.indexOf('github.com') === -1) {
-    githubRepo = 'http://github.com/' + githubRepo;
-  }
+    //If github repo isn't given, try to read it from the package file
+    if (!type && pkg) {
+        if (pkg.repository) {
+            type = 3;
+            repoUrl = pkg.repository.url;
+        } else if (pkg.homepage) {
+            //If it's a bitbucket page or github page (but not a *.github.(io|com) page)
+            if (pkg.homepage.indexOf('github.com') > -1 && !pkg.homepage.match(/\.github\.(com|io)/) || pkg.homepage.indexOf('bitbucket.org') > -1) {
+                type = 3;
+                repoUrl = pkg.homepage;
+            }
+        }
+    }
 
-  githubRepo = githubRepo
-      .replace(/^git:\/\//, 'http://') //get rid of git://
-      .replace(/\/$/, '') //get rid of trailing slash
-      .replace(/\.git$/, ''); //get rid of trailing .git
+    //Check if the URL in the package.json is a bitbucket or a github url
+    if (type == 3) {
+        if (repoUrl.indexOf('github.com') > -1) {
+            type = 1;
+        } else if (repoUrl.indexOf('bitbucket.org') > -1) {
+            type = 2;
+        }
+    }
 
-  return githubRepo;
+    //User could set option eg 'github: "btford/grunt-conventional-changelog'
+    if (type === 1 && repoUrl.indexOf('github.com') === -1) {
+        repoUrl = 'http://github.com/' + repoUrl;
+    } else if (type === 2 && repoUrl.indexOf('bitbucket.org') === -1) {
+        repoUrl = 'http://bitbucket.org/' + repoUrl;
+    }
+
+    repoUrl = repoUrl
+        .replace(/^git:\/\//, 'http://') //get rid of git://
+    .replace(/\/$/, '') //get rid of trailing slash
+    .replace(/\.git$/, ''); //get rid of trailing .git
+
+    return [type, repoUrl];
 };
 
 
-module.exports = function (grunt) {
+module.exports = function(grunt) {
 
-  var DESC = 'Generate a changelog from git metadata.';
-  grunt.registerTask('changelog', DESC, function () {
+    var DESC = 'Generate a changelog from git metadata.';
+    grunt.registerTask('changelog', DESC, function() {
 
-    var done = this.async();
-    var options = this.options({
-      dest: 'CHANGELOG.md',
-      prepend: true,  // false to append
-      github: null,   // default from package.json
-      version: null,  // default value from package.json
-      editor: null    // 'sublime -w'
-    });
-
-    var pkg = grunt.config('pkg') || grunt.file.readJSON('package.json');
-    var githubRepo = figureOutGithubRepo(options.github, pkg);
-    var newVersion = options.version || pkg.version;
-
-
-    // generate changelog
-    changelog.generate(githubRepo, 'v' + newVersion).then(function(data) {
-      var currentLog = grunt.file.exists(options.dest) ? grunt.file.read(options.dest) : '';
-      grunt.file.write(options.dest, options.prepend ? data + currentLog : currentLog + data);
-
-      if (options.editor) {
-        exec(options.editor + ' ' + options.dest, function(err) {
-          if (err) {
-            return grunt.fatal('Can not generate changelog.');
-          }
-
-          grunt.log.ok(options.dest + ' updated');
-          done();
+        var done = this.async();
+        var options = this.options({
+            dest: 'CHANGELOG.md',
+            prepend: true, // false to append
+            github: null, // default from package.json
+            bitbucket: null, // default from package.json
+            version: null, // default value from package.json
+            editor: null // 'sublime -w'
         });
-      } else {
-        grunt.log.ok(options.dest + ' updated');
-        done();
-      }
+
+        var pkg = grunt.config('pkg') || grunt.file.readJSON('package.json');
+        var repoUrlAndType = figureOutRepo(options.github, options.bitbucket, pkg);
+        var newVersion = options.version || pkg.version;
+
+
+        // generate changelog
+        changelog.generate(repoUrlAndType[0], repoUrlAndType[1], 'v' + newVersion).then(function(data) {
+            var currentLog = grunt.file.exists(options.dest) ? grunt.file.read(options.dest) : '';
+            grunt.file.write(options.dest, options.prepend ? data + currentLog : currentLog + data);
+
+            if (options.editor) {
+                exec(options.editor + ' ' + options.dest, function(err) {
+                    if (err) {
+                        return grunt.fatal('Can not generate changelog.');
+                    }
+
+                    grunt.log.ok(options.dest + ' updated');
+                    done();
+                });
+            } else {
+                grunt.log.ok(options.dest + ' updated');
+                done();
+            }
+        });
     });
-  });
 };
