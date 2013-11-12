21c6778b6204849c2a64db4bd6c79e887bf6b3b6
Jonas Einhorn <jonas.wittlin@daseinhorn.ch>
2013-11-12 22:27:08 +0100

    a subject without a : is still ok :)

--- tasks/changelog.js
+++ tasks/changelog.js
@@ -1,99 +1,252 @@
-var exec = require('child_process').exec;
-var changelog = require('../lib/changelog');
+// TODO(vojta): report errors, currently Q silence everything which really sucks
+// TODO(vojta): use grunt logger
+// TODO(vojta): nicer breaking changes (https://github.com/angular/angular.js/commit/07a58dd7669431d33b61f8c3213c31eff744d02a)
+// TODO(vojta): ignore "Merge pull request..." messages, or ignore them in git log ? (--no-merges)
+
+var child = require('child_process');
+var util = require('util');
+var q = require('qq');
 
 
-// TODO: clean this up and write tests for it
-var figureOutRepo = function(githubRepo, bitbucketRepo, pkg) {
-    //Check if the user gave either a github or a bitbucket url
-    var type = 0,
-        repoUrl = "";
-    if (githubRepo) {
-        console.log("1");
-        repoUrl = githubRepo;
-        bitbucketRepo = "";
-    } else if (bitbucketRepo) {
-        type = 2;
-        repoUrl = bitbucketRepo;
-        githubRepo = "";
-    }
+var GIT_LOG_CMD = 'git log --grep="%s" -E --format=%s %s..HEAD';
+var GIT_TAG_CMD = 'git describe --tags --abbrev=0';
 
-    //If github repo isn't given, try to read it from the package file
-    if (!type && pkg) {
-        if (pkg.repository) {
-            type = 3;
-            repoUrl = pkg.repository.url;
-        } else if (pkg.homepage) {
-            //If it's a bitbucket page or github page (but not a *.github.(io|com) page)
-            if (pkg.homepage.indexOf('github.com') > -1 && !pkg.homepage.match(/\.github\.(com|io)/) || pkg.homepage.indexOf('bitbucket.org') > -1) {
-                type = 3;
-                repoUrl = pkg.homepage;
-            }
-        }
-    }
+var EMPTY_COMPONENT = '$$';
+var MAX_SUBJECT_LENGTH = 80;
 
-    //Check if the URL in the package.json is a bitbucket or a github url
-    if (type == 3) {
-        if (repoUrl.indexOf('github.com') > -1) {
-            type = 1;
-        } else if (repoUrl.indexOf('bitbucket.org') > -1) {
-            type = 2;
-        }
-    }
+var PATTERN = /^(\w*)(\(([\w\$\.\-\*]*)\))?\: (.*)$/;
+var PATTERN2 = /^(\w*)(\(([\w\$\.\-\*]*)\)) (.*)$/;
 
-    //User could set option eg 'github: "btford/grunt-conventional-changelog'
-    if (type === 1 && repoUrl.indexOf('github.com') === -1) {
-        repoUrl = 'http://github.com/' + repoUrl;
-    } else if (type === 2 && repoUrl.indexOf('bitbucket.org') === -1) {
-        repoUrl = 'http://bitbucket.org/' + repoUrl;
-    }
 
-    repoUrl = repoUrl
-        .replace(/^git:\/\//, 'http://') //get rid of git://
-    .replace(/\/$/, '') //get rid of trailing slash
-    .replace(/\.git$/, ''); //get rid of trailing .git
+var warn = function() {
+    console.log('WARNING:', util.format.apply(null, arguments));
+};
 
-    return [type, repoUrl];
+var log = function() {
+    console.log(util.format.apply(null, arguments));
 };
 
 
-module.exports = function(grunt) {
+var parseRawCommit = function(raw) {
+    if (!raw) {
+        return null;
+    }
 
-    var DESC = 'Generate a changelog from git metadata.';
-    grunt.registerTask('changelog', DESC, function() {
+    var lines = raw.split('\n');
+    var msg = {}, match;
 
-        var done = this.async();
-        var options = this.options({
-            dest: 'CHANGELOG.md',
-            prepend: true, // false to append
-            github: null, // default from package.json
-            bitbucket: null, // default from package.json
-            version: null, // default value from package.json
-            editor: null // 'sublime -w'
+    msg.hash = lines.shift();
+    msg.subject = lines.shift();
+    msg.closes = [];
+    msg.breaks = [];
+
+    msg.subject = msg.subject.replace(/\s*(?:Closes|Fixes)\s#(\d+)/, function(_, i) {
+        msg.closes.push(parseInt(i, 10));
+        return '';
+    });
+
+
+    lines.forEach(function(line) {
+        match = line.match(/(?:Closes|Fixes)\s((?:#\d+(?:\,\s)?)+)/);
+
+        if (match) {
+            match[1].replace(/[\s#]/g, '').split(',').forEach(function(i) {
+                msg.closes.push(parseInt(i, 10));
+            });
+        }
+    });
+
+    match = raw.match(/BREAKING CHANGE:\s([\s\S]*)/);
+    if (match) {
+        msg.breaks.push(match[1]);
+    }
+
+
+    msg.body = lines.join('\n');
+    match = msg.subject.match(PATTERN);
+
+    if (!match) {
+        match = msg.subject.match(PATTERN2);
+    }
+
+    if (!match || !match[1] || !match[4]) {
+        warn('Incorrect message: %s %s', msg.hash, msg.subject, match);
+        return null;
+    }
+
+    if (match[4].length > MAX_SUBJECT_LENGTH) {
+        warn('Too long subject: %s %s', msg.hash, msg.subject);
+        match[4] = match[4].substr(0, MAX_SUBJECT_LENGTH);
+    }
+
+    msg.type = match[1];
+    msg.component = match[3];
+    msg.subject = match[4];
+
+    return msg;
+};
+
+
+var currentDate = function() {
+    var now = new Date();
+    var pad = function(i) {
+        return ('0' + i).substr(-2);
+    };
+
+    return util.format('%d-%s-%s', now.getFullYear(), pad(now.getMonth() + 1), pad(now.getDate()));
+};
+
+
+var readGitLog = function(grep, from) {
+    log('Reading git log since', from);
+
+    var deffered = q.defer();
+
+    child.exec(util.format(GIT_LOG_CMD, grep, '%H%n%s%n%b%n==END==', from), function(code, stdout) {
+        var commits = [];
+
+        stdout.split('\n==END==\n').forEach(function(rawCommit) {
+            var commit = parseRawCommit(rawCommit);
+            if (commit) {
+                commits.push(commit);
+            }
         });
 
-        var pkg = grunt.config('pkg') || grunt.file.readJSON('package.json');
-        var repoUrlAndType = figureOutRepo(options.github, options.bitbucket, pkg);
-        var newVersion = options.version || pkg.version;
+        log('Parsed %s commits', commits.length);
+
+        deffered.resolve(commits);
+    });
+
+    return deffered.promise;
+};
 
 
-        // generate changelog
-        changelog.generate(repoUrlAndType[0], repoUrlAndType[1], 'v' + newVersion).then(function(data) {
-            var currentLog = grunt.file.exists(options.dest) ? grunt.file.read(options.dest) : '';
-            grunt.file.write(options.dest, options.prepend ? data + currentLog : currentLog + data);
+var PATCH_HEADER_TPL = '<a name="%s"></a>\n### %s (%s)\n\n';
+var MINOR_HEADER_TPL = '<a name="%s"></a>\n## %s (%s)\n\n';
+var LINK_ISSUE = '[#%s](%s/issues/%s)';
+var LINK_COMMIT = '[%s](%s/commit/%s)';
+var LINK_COMMIT_BITBUCKET = '[%s](%s/commits/%s)';
 
-            if (options.editor) {
-                exec(options.editor + ' ' + options.dest, function(err) {
-                    if (err) {
-                        return grunt.fatal('Can not generate changelog.');
-                    }
+var Writer = function(stream, repoType, repoUrl) {
+    var linkToIssue = function(issue) {
+        return util.format(LINK_ISSUE, issue, repoUrl, issue);
+    };
 
-                    grunt.log.ok(options.dest + ' updated');
-                    done();
-                });
-            } else {
-                grunt.log.ok(options.dest + ' updated');
-                done();
+    var linkToCommit = function(hash) {
+        if (repoType != 2) {
+            return util.format(LINK_COMMIT, hash.substr(0, 8), repoUrl, hash);
+
+        } else {
+            return util.format(LINK_COMMIT_BITBUCKET, hash.substr(0, 8), repoUrl, hash);
+
+        }
+    };
+
+    this.header = function(version) {
+        var header = version.split('.')[2] === '0' ? MINOR_HEADER_TPL : PATCH_HEADER_TPL;
+        stream.write(util.format(header, version, version, currentDate()));
+    };
+
+    this.section = function(title, section) {
+        var components = Object.getOwnPropertyNames(section).sort();
+
+        if (!components.length) {
+            return;
+        }
+
+        stream.write(util.format('\n#### %s\n\n', title));
+
+        components.forEach(function(name) {
+            var prefix = '*';
+            var nested = section[name].length > 1;
+
+            if (name !== EMPTY_COMPONENT) {
+                if (nested) {
+                    stream.write(util.format('* **%s:**\n', name));
+                    prefix = '  *';
+                } else {
+                    prefix = util.format('* **%s:**', name);
+                }
             }
+
+            section[name].forEach(function(commit) {
+                stream.write(util.format('%s %s (%s', prefix, commit.subject, linkToCommit(commit.hash)));
+                if (commit.closes.length) {
+                    stream.write(', closes ' + commit.closes.map(linkToIssue).join(', '));
+                }
+                stream.write(')\n');
+            });
+        });
+
+        stream.write('\n');
+    };
+};
+
+var writeChangelog = function(writer, commits, version) {
+    var sections = {
+        fix: {},
+        feat: {},
+        breaks: {}
+    };
+
+    commits.forEach(function(commit) {
+        var section = sections[commit.type];
+        var component = commit.component || EMPTY_COMPONENT;
+
+        if (section) {
+            section[component] = section[component] || [];
+            section[component].push(commit);
+        }
+
+        commit.breaks.forEach(function(breakMsg) {
+            sections.breaks[EMPTY_COMPONENT] = sections.breaks[EMPTY_COMPONENT] || [];
+
+            sections.breaks[EMPTY_COMPONENT].push({
+                subject: breakMsg,
+                hash: commit.hash,
+                closes: []
+            });
+        });
+    });
+
+    writer.header(version);
+    writer.section('Bug Fixes', sections.fix);
+    writer.section('Features', sections.feat);
+    writer.section('Breaking Changes', sections.breaks);
+};
+
+
+var getPreviousTag = function() {
+    var deffered = q.defer();
+    child.exec(GIT_TAG_CMD, function(code, stdout) {
+        if (code) {
+            deffered.reject('Cannot get the previous tag.');
+        } else {
+            deffered.resolve(stdout.replace('\n', ''));
+        }
+    });
+    return deffered.promise;
+};
+
+
+// PUBLIC API
+exports.generate = function(repoType, repoUrl, version) {
+    var buffer = {
+        data: '',
+        write: function(str) {
+            this.data += str;
+        }
+    };
+    var writer = new Writer(buffer, repoType, repoUrl);
+
+    return getPreviousTag().then(function(tag) {
+        return readGitLog('^fix|^feat|BREAKING', tag).then(function(commits) {
+            writeChangelog(writer, commits, version);
+            return buffer.data;
         });
     });
 };
+
+
+// publish for testing
+exports.parseRawCommit = parseRawCommit;
